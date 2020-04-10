0989c78c5f9dd6465b70812a51732f67970e1c56
Soares Chen <soares.chen@gmail.com>
2020-04-10 15:41:16 +0800

    Update subproject commits

--- source/remote.nix
+++ source/remote.nix
@@ -1,12 +1,12 @@
 let
   quasi-params = builtins.fetchGit {
     url = "https://github.com/maybevoid/quasi-params.git";
-    rev = "";
+    rev = "3e2332aa2a380e1d6bc7bc226cf50819b2e3cfd5";
   };
 
   casimir = builtins.fetchGit {
     url = "https://github.com/maybevoid/casimir.git";
-    rev = "";
+    rev = "0266b5a7348db0b2352bb5d8f6bb5734d95cdc04";
   };
 in
 { inherit
