13e37dbe0975b3b7b0f664b2e1e3e4bed8ee8c70
Soares Chen <soares.chen@gmail.com>
2020-06-03 21:30:39 +0200

    Update nixpkgs

--- haskell-packages/8.10.nix
+++ haskell-packages/8.10.nix
@@ -11,58 +11,6 @@ let
     overrides = pkgs.lib.composeExtensions
       (old.overrides or (_: _: {}))
       (self: super: {
-
-        doctest = haskellLib.dontCheck
-          ( super.callPackage
-            ({ mkDerivation, base, base-compat, code-page, deepseq, directory
-            , filepath, ghc, ghc-paths, hspec, HUnit, mockery, process
-            , QuickCheck, setenv, silently, stringbuilder, syb, transformers
-            }:
-            mkDerivation {
-              pname = "doctest";
-              version = "0.16.2";
-              src = builtins.fetchGit {
-                url = "https://github.com/sol/doctest.git";
-                rev = "39eae983aa5f2e19cd797a559b04f044715d63e8";
-              };
-              isLibrary = true;
-              isExecutable = true;
-              libraryHaskellDepends = [
-                base base-compat code-page deepseq directory filepath ghc ghc-paths
-                process syb transformers
-              ];
-              executableHaskellDepends = [
-                base base-compat code-page deepseq directory filepath ghc ghc-paths
-                process syb transformers
-              ];
-              testHaskellDepends = [
-                base base-compat code-page deepseq directory filepath ghc ghc-paths
-                hspec HUnit mockery process QuickCheck setenv silently
-                stringbuilder syb transformers
-              ];
-              description = "Test interactive Haskell examples";
-              license = stdenv.lib.licenses.mit;
-            }) {}
-          )
-        ;
-
-        lifted-async = haskellLib.appendPatch
-          ( super.callHackage
-              "lifted-async"
-              "0.10.0.5"
-              {}
-          )
-          ../patches/lifted-async.patch
-        ;
-
-        language-haskell-extract = haskellLib.appendPatch
-          ( util.callGitPackage super
-              "language-haskell-extract"
-              "https://github.com/finnsson/template-helper.git"
-              "9047b3793aac904052dcb3f1704b53d6d41d2374"
-          )
-          ../patches/language-haskell-extract.patch
-        ;
       });
     });
 in
--- source/nixpkgs.nix
+++ source/nixpkgs.nix
@@ -1,4 +1,4 @@
 builtins.fetchGit {
   url = "https://github.com/NixOS/nixpkgs.git";
-  rev = "9242acfbbc622ce72181f518966afc9ca7e2bf91";
+  rev = "30d9df50ecd0cdb32afcf02e8012e08fa1bb7c6b";
 }
